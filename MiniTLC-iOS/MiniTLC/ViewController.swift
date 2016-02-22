08c20a55ce08e882e33f16929cab98a54838b410
DeKoServidoni <deko.puc@gmail.com>
2016-02-21 22:52:47 -0300

    [iOS] Add the SWRevealViewController
    Added the left swipe menu implemented by the SWRevealView library

--- MiniTLC-iOS/MiniTLC/ViewController.swift
+++ MiniTLC-iOS/MiniTLC/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 

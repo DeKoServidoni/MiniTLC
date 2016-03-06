8626d65ad7c9a88064e2591873074124cedcdeed
DeKoServidoni <deko.puc@gmail.com>
2016-03-05 22:13:43 -0300

    [iOS] Navigation menu item actions
    - Implemented the Talk with Us item and action: Call the favorite email
    provider of the user and send to the coordination email

--- MiniTLC-iOS/MiniTLC/MiniTLC/SidePanelViewController.swift
+++ MiniTLC-iOS/MiniTLC/MiniTLC/SidePanelViewController.swift
@@ -12,34 +12,37 @@ import UIKit
 class SidePanelViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Class attributes
+    
     var menu: [String] = ["Tirar foto!","Fale conosco!", "Sobre"]
     var icon: [String] = ["icon_camera", "icon_talk_with_us", "icon_about"]
     
+    // MARK: Class constants
+    
     let NUMBER_OF_SECTIONS = 2
+    
+    let SECTION_1_HIGHT: CGFloat = 0
+    let SECTION_2_HIGHT: CGFloat = 2
     
     // MARK: Table view delegate functions
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (section == 0) ? 1 : 2
+        let quantitySection1 = menu.count - 2
+        let quantitySection2 = menu.count - quantitySection1
+        
+        return (section == 0) ? quantitySection1 : quantitySection2
     }
     
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return (section == 1) ? "TESTE" : ""
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return (section == 0) ? SECTION_1_HIGHT : SECTION_2_HIGHT
     }
-    
-    //func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        //
-    //}
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return NUMBER_OF_SECTIONS
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let identifier = "MenuItemIdentifier"
-        
-        var cell = tableView.dequeueReusableCellWithIdentifier(identifier) as? SlideMenuCell
+
+        var cell = tableView.dequeueReusableCellWithIdentifier("MenuItemIdentifier") as? SlideMenuCell
         
         if cell == nil {
             let array = NSBundle.mainBundle().loadNibNamed("SlideMenuCell", owner: self, options: nil)
@@ -55,6 +58,34 @@ class SidePanelViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-       print("CLICKED!")
+       
+        if indexPath.section == 0 {
+            handleClickInFirstSection(onRow: indexPath.row)
+        } else if indexPath.section == 1 {
+            handleClickInSecondSection(onRow: indexPath.row)
+        }
+    }
+    
+    // MARK: Private functions
+    
+    // handle the click in the rows of the first section
+    private func handleClickInFirstSection(onRow row: Int) {
+        /** TODO **/
+    }
+    
+    // handle the click in the rows of the second section
+    private func handleClickInSecondSection(onRow row: Int) {
+        
+        switch row {
+            
+            case 0: /* Talk with US */
+                let url = NSURL(string: "mailto:mini@tlccampinas.com.br")
+                UIApplication.sharedApplication().openURL(url!)
+                break
+            
+            default:
+                // do nothing
+                break
+        }
     }
 }

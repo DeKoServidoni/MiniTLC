14b173e529681623b6c2c09312c69ef30a8a53f5
DeKoServidoni <deko.puc@gmail.com>
2016-08-14 19:36:43 -0300

    Remove the API from this repository

--- TLC-API/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-	namespace :api, defaults: { format: :json }, path: '/'  do
-		namespace :v1 do
-			resources :events, :only => [:index, :create, :destroy]
-    	end
-  end 
-  
-end
